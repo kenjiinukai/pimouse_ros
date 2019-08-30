1300e548fb9b1f995529a91ef02673eef20aeb16
kenjiinukai <kenji.inki@gmail.com>
2019-08-30 11:03:08 +0900

    Fix a test script bug

--- scripts/motor1.py
+++ scripts/motor1.py
@@ -11,4 +11,9 @@ class Motor():
 		rospy.on_shutdown(self.set_power)
 		self.sub_raw = rospy.Subscriber('motor_raw', MotorFreqs, self.callback_raw_freq)
 		self.sub_cmd_vel = rospy.Subscriber('cmd_vel', Twist, self.callback_cmd_vel)
+		self.last_time = rospy.Time.now()
+		self.using_cmd_vel = False
+
+	def	set_power(self,onoff=False):
+		en = "/dev/rtmotoren0"
 		
