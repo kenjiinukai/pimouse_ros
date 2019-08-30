0444086ab51325408d35a960949cedb9e03745fe
kenjiinukai <kenji.inki@gmail.com>
2019-08-30 10:46:25 +0900

    Fix a test script bug

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+[#!/usr/bin/env python
 #encoding: utf8
 import unittest, rostest
 import rosnode, rospy
 import time
 from pimouse_ros.msg import LightSensorValues
 
-class LightsensorTest(unittest,TestCase):
+class LightsensorTest(unittest.TestCase):
 	def	setUp(self):
 		self.count = 0
 		rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
