dbc061842339c6516ef39417e260739517994259
Kenji Inukai <kenji.inki@gmail.com>
2020-03-09 01:19:47 +0000

    Fix buzzer test script

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -41,7 +41,7 @@ class BuzzerTest(unittest.TestCase):
 		###preemption###
 		self.device_values = []
 		self.client.send_goal(goal,feedback_cb=self.feedback_cb)
-		self.client.wait_for_result(rospy.uration.from_sec(0.5))
+		self.client.wait_for_result(rospy.Duration.from_sec(0.5))
 		
 		self.assertFalse(self.client.get_result(), "stop is requested but return true")
 		self.assertFalse(goal.freqs == self.device_values,"not stopped")
